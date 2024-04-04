3fc76582b9027668c391af494d8a52a84fc41a81
Mood__Panda <andre-enju@ukr.net>
2024-04-04 15:44:56 +0300

    1. кастомна форма валідації ще готова

--- custom-form-validation/index.js
+++ custom-form-validation/index.js
@@ -24,18 +24,32 @@ function validation(form) {
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return regex.test(email);
     }
+    
+    function validateTelNumber(tel) {
+        const regex = /^[+]?[0-9]{8,13}$/;
+        return regex.test(tel);
+    }
+
+    function validatePassword(password) {
+        const regex = /^[a-zA-Z0-9_-]{8,}$/;
+        return regex.test(password);
+    }
 
     let result = true;
 
     form.querySelectorAll("input").forEach((input) => {
         removeError(input);
         if (input.value.trim() == "") {
-            createError(input, "Поле не заповнене");
+            createError(input, "Поле має бути заповнене");
             result = false;
-        }
-        if (input.type === "email" && !validateEmail(input.value)) {
-            // createError(input, "Неправильний формат електронної пошти");
+        } else if (input.id === "email" && !validateEmail(input.value)) {
+            createError(input, "Неправильний формат електронної пошти");
             result = false;
+        } else if (input.id === "tel" && !validateTelNumber(input.value)) {
+            createError(input, "Неправильний формат номера телефону");
+            result = false;
+        } else if (input.id === "password" && !validatePassword(input.value)) {
+            createError(input, "Неправильний формат паролю");
         }
     });
 
